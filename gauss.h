4d49376d5d39c64892d417936d3e66c0a630595d
Gleb Grenkin <glebgrenkin@gmail.com>
2013-06-14 20:16:09 +1200

    Create gauss.h

---
+++ gauss.h
@@ -0,0 +1,9 @@
+#ifndef GAUSS_H_INCLUDED
+#define GAUSS_H_INCLUDED
+
+#include <vector>
+
+std::vector<std::vector<double> > Gauss(
+  std::vector<std::vector<double> > a, std::vector<double> b);
+
+#endif // GAUSS_H_INCLUDED
